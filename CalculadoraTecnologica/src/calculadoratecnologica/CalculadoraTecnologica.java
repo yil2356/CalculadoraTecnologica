861afcf6b190b6a744ccb59ec3da501fa1532fd5
Alexander pechene <Alexanderpechene79@gmail.com>
2026-05-30 11:47:01 -0500

    agrego, saludo de bienvenida

---
+++ CalculadoraTecnologica/src/calculadoratecnologica/CalculadoraTecnologica.java
@@ -0,0 +1,10 @@
+
+package calculadoratecnologica;
+
+public class CalculadoraTecnologica {
+
+    public static void main(String[] args) {
+        System.out.println("Hola, Bienvenido Alexander Pechene");
+    }
+    
+}
